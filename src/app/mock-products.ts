28258640f2c59cfe9c047dab2a46b2bd0538d718
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2021-11-22 17:11:03 +0600

    feat(products): Added products component

---
+++ src/app/mock-products.ts
@@ -0,0 +1,14 @@
+import { Product} from "./product";
+
+export const PRODUCTS: Product[] = [
+  { id: 11, model: 'MacBook Pro 11' },
+  { id: 12, model: 'MacBook Pro 12' },
+  { id: 13, model: 'MacBook Pro 13' },
+  { id: 14, model: 'MacBook Pro 14' },
+  { id: 15, model: 'MacBook Pro 15' },
+  { id: 16, model: 'MacBook Pro 16' },
+  { id: 17, model: 'MacBook Pro 17' },
+  { id: 18, model: 'MacBook Pro 18' },
+  { id: 19, model: 'MacBook Pro 19' },
+  { id: 20, model: 'MacBook Pro 20' }
+]
